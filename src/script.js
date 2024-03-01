a053e341a8a7f84c797daff92b408407e240c887
Matsuiiii <161684510+Matsuiiii@users.noreply.github.com>
2024-03-01 17:10:15 +0900

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,13 +1,5 @@
-var img;
-
-    function redfish(){
-    img = document.getElementById(“image_file”);
-    img.src = “redfishcat”;
-    }
-
-    function bluefish(){
-    img = document.getElementById(“image_file”);
-    img.src = “bluefishcat”;
-    }
-
-    
+//"bluefish"というIDを取得して、クリックしたらfunctionを実行する
+document.getElementById("bluefish").onclick = function(){
+    //"pics"というIDを取得して、そのsrcを"bluefishcat.jpg"に変更する
+    document.getElementById("pics").src="bluefishcat.jpg";
+  }
