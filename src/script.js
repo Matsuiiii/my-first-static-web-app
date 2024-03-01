997821d3e3b4d5a61599b2ad268f97a88e3e2f25
Matsuiiii <161684510+Matsuiiii@users.noreply.github.com>
2024-03-01 11:59:25 +0900

    Create script.js
    占い途中

---
+++ src/script.js
@@ -0,0 +1,12 @@
+const colors = [
+    "赤　りんごの皮を食べましょう。ポリフェノールが豊富です♬",
+    "紫　ブルーベリーで目の疲れをリセット！",
+    "黄色",
+    // 思いついたら追加
+];
+
+function dokidoki() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quoteElement = document.getElementById("uranai");
+    quoteElement.textContent = quotes[randomIndex];
+}
