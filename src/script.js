6633251d4a15f33f9e325781c2c43e3dd2d33cc7
Matsuiiii <161684510+Matsuiiii@users.noreply.github.com>
2024-03-01 17:14:31 +0900

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 //"bluefish"というIDを取得して、クリックしたらfunctionを実行する
 document.getElementById("bluefish").onclick = function(){
     //"pics"というIDを取得して、そのsrcを"bluefishcat.jpg"に変更する
-    document.getElementById("pics").src="bluefishcat.jpg";
+    document.getElementById("pics").src="Bluefishcat.jpg";
   }
